34a6ec1d0897efc15fc95a91c7ce88eec963c599
Peter Forez <Peter.Forez@gmail.com>
2021-07-21 07:10:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,24 @@ using std::vector;
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-string hasData(string s) {
+string hasData(string s) 
+{
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.find_first_of("]");
-  if (found_null != string::npos) {
+  if (found_null != string::npos) 
+  {
     return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
+  } 
+  else if (b1 != string::npos && b2 != string::npos) 
+  {
     return s.substr(b1, b2 - b1 + 1);
   }
   return "";
 }
 
-int main() {
+int main() 
+{
   uWS::Hub h;
 
   // Set up parameters here
@@ -39,7 +44,8 @@ int main() {
 
   // Read map data
   Map map;
-  if (!read_map_data("../data/map_data.txt", map)) {
+  if (!read_map_data("../data/map_data.txt", map)) 
+  {
     std::cout << "Error: Could not open map file" << std::endl;
     return -1;
   }
@@ -49,11 +55,13 @@ int main() {
 
   h.onMessage([&pf,&map,&delta_t,&sensor_range,&sigma_pos,&sigma_landmark]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
-               uWS::OpCode opCode) {
+               uWS::OpCode opCode) 
+  {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+    if (length && length > 2 && data[0] == '4' && data[1] == '2') 
+    {
       auto s = hasData(string(data));
 
       if (s != "") {
@@ -61,9 +69,11 @@ int main() {
 
         string event = j[0].get<string>();
         
-        if (event == "telemetry") {
+        if (event == "telemetry") 
+        {
           // j[1] is the data JSON object
-          if (!pf.initialized()) {
+          if (!pf.initialized()) 
+          {
             // Sense noisy position data from the simulator
             double sense_x = std::stod(j[1]["sense_x"].get<string>());
             double sense_y = std::stod(j[1]["sense_y"].get<string>());
@@ -100,7 +110,8 @@ int main() {
           std::istream_iterator<float>(),
           std::back_inserter(y_sense));
 
-          for (int i = 0; i < x_sense.size(); ++i) {
+          for (int i = 0; i < x_sense.size(); ++i) 
+          {
             LandmarkObs obs;
             obs.x = x_sense[i];
             obs.y = y_sense[i];
@@ -118,8 +129,10 @@ int main() {
           double highest_weight = -1.0;
           Particle best_particle;
           double weight_sum = 0.0;
-          for (int i = 0; i < num_particles; ++i) {
-            if (particles[i].weight > highest_weight) {
+          for (int i = 0; i < num_particles; ++i) 
+          {
+            if (particles[i].weight > highest_weight) 
+            {
               highest_weight = particles[i].weight;
               best_particle = particles[i];
             }
@@ -145,27 +158,33 @@ int main() {
           // std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }  // end "telemetry" if
-      } else {
+      } 
+      else 
+      {
         string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
     }  // end websocket message if
   }); // end h.onMessage
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) 
+  {
     std::cout << "Connected!!!" << std::endl;
   });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
-                         char *message, size_t length) {
+                         char *message, size_t length) 
+  {
     ws.close();
     std::cout << "Disconnected" << std::endl;
   });
 
   int port = 4567;
-  if (h.listen(port)) {
+  if (h.listen(port)) 
+  {
     std::cout << "Listening to port " << port << std::endl;
-  } else {
+  } else 
+  {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
